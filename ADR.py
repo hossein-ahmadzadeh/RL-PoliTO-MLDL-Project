1b5e8e6d3599ed66cada4e7bb5fff4671f81b1bc
Aso <hossein.ahmadzadeh.edu@gmail.com>
2025-06-26 19:11:35 +0000

    fixed adr

--- ADR.py
+++ ADR.py
@@ -1,165 +1,123 @@
+# ADR.py
+
 import numpy as np
 from stable_baselines3.common.callbacks import BaseCallback
 import wandb
 
-EPS = 2e-15
+EPS = 1e-8 # Use a small epsilon to avoid zero-width or negative distributions
 
 class AutomaticDomainRandomization():
-        def __init__(self, init_params: dict, p_b = 0.5, m = 50, delta = 0.05, thresholds = [1000, 1600] ) -> None:
-            self.init_params = init_params # initial values of each part of the body
-            self.thresholds = thresholds
-            self.delta = delta
-            self.m = m # number of episodes, dimension of buffer
-            self.bounds = self._init_bounds()
-            self.p_b = p_b
-            self.thigh_mass = None
-            self.leg_mass = None
-            self.foot_mass = None
-            self.rewards = []
-            self.partOfBody = ['thigh', 'leg', 'foot']
-            self.buffer = {
-                "thigh_low": [],
-                "thigh_high": [],
-                "leg_low": [],
-                "leg_high": [],
-                "foot_low": [],
-                "foot_high": []
-            }
-            self.keys = list(self.buffer.keys())
-            self.performances = {
-                "thigh_low": [],
-                "thigh_high": [],
-                "leg_low": [],
-                "leg_high": [],
-                "foot_low": [],
-                "foot_high": []
-            }
-            self.increments = []
+    """
+    Implements the Automatic Domain Randomization algorithm.
+    It dynamically adjusts the distribution of physics parameters (masses)
+    of the environment based on the agent's performance.
+    """
+    def __init__(self, init_params: dict, p_b=0.5, m=50, delta=0.05, thresholds=[1000, 1600]):
+        self.init_params = init_params
+        self.thresholds = thresholds
+        self.delta = delta
+        self.m = m  # Data buffer size for performance evaluation
+        self.p_b = p_b # Probability of sampling a boundary value
+        self.bounds = self._init_bounds()
+        self.partOfBody = ['thigh', 'leg', 'foot']
+        
+        # Buffers to store rewards for boundary values
+        self.buffer = {f"{part}_{bound}": [] for part in self.partOfBody for bound in ["low", "high"]}
+        
+        # Store historical performance for analysis
+        self.performances = {key: [] for key in self.buffer.keys()}
 
-        def _init_bounds(self):
-            dict = {
-                "thigh_low": self.init_params['thigh']-EPS,
-                "thigh_high": self.init_params['thigh']+EPS,
-                "leg_low": self.init_params['leg']-EPS,
-                "leg_high": self.init_params['leg']+EPS,
-                "foot_low": self.init_params['foot']-EPS,
-                "foot_high": self.init_params['foot']+EPS
-            }
-            return dict
-        def _constant(self, *args):
-            return self.delta
-        def compute_entropy(self):
-            range_thigh = abs(self.bounds['thigh_high'] - self.bounds['thigh_low'])
-            range_leg = abs(self.bounds['leg_high'] - self.bounds['leg_low'])
-            range_foot = abs(self.bounds['foot_high'] - self.bounds['foot_low'])
-            entropy = np.log([range_thigh, range_leg, range_foot]).mean()
-            return entropy
-        
-        def append_reward_bodypart(self, partOfBody: str, reward: float):
-            self.buffer[partOfBody].append(reward)
-            return None
-        
-        def evaluate_performance(self, partOfBody: str): # compute the performance of a single part of body as the mean of all rewards
-            performance = np.mean(np.array(self.buffer[partOfBody]))
+    def _init_bounds(self):
+        """Initializes randomization bounds as a narrow distribution around initial parameters."""
+        return {
+            "thigh_low": self.init_params['thigh'] - EPS, "thigh_high": self.init_params['thigh'] + EPS,
+            "leg_low": self.init_params['leg'] - EPS, "leg_high": self.init_params['leg'] + EPS,
+            "foot_low": self.init_params['foot'] - EPS, "foot_high": self.init_params['foot'] + EPS
+        }
+
+    def compute_entropy(self):
+        """Computes the entropy of the current randomization distribution."""
+        ranges = [
+            self.bounds['thigh_high'] - self.bounds['thigh_low'],
+            self.bounds['leg_high'] - self.bounds['leg_low'],
+            self.bounds['foot_high'] - self.bounds['foot_low']
+        ]
+        entropy = np.log(np.array(ranges) + EPS).mean()
+        return entropy
+    
+    def update_ADR(self, partOfBody: str, current_timestep: int):
+        """Updates ADR bounds if the buffer for a given boundary is full."""
+        if len(self.buffer[partOfBody]) >= self.m:
+            body_part, high_or_low = partOfBody.split('_')
+            
+            performance = np.mean(self.buffer[partOfBody])
             self.buffer[partOfBody].clear()
-            return performance
+            self.performances[partOfBody].append(performance)
+            wandb.log({f"adr_performance/{partOfBody}": performance}, step=current_timestep)
+
+            if performance >= self.thresholds[1]:  # High performance -> expand
+                if high_or_low == "high": self.increase(partOfBody)
+                else: self.decrease(partOfBody)
+            elif performance <= self.thresholds[0]:  # Low performance -> contract
+                if high_or_low == "low": self.increase(partOfBody)
+                else: self.decrease(partOfBody)
         
-        def update_ADR(self, partOfBody: str):
-            if len(self.buffer[partOfBody]) >= self.m:
-                highOrLow = partOfBody.split('_')[1]
-                performance = self.evaluate_performance(partOfBody)
-                self.performances[partOfBody].append(performance)
-
-                if performance >= self.thresholds[1]:
-                    if highOrLow == "high":
-                        self.increase(partOfBody)
-                    else:
-                        self.decrease(partOfBody)
-                if performance <= self.thresholds[0]:
-                    if highOrLow == "low":
-                        self.increase(partOfBody)
-                    else:
-                        self.decrease(partOfBody)
-            wandb.log({"bound" : self.get_bounds()})
+        wandb.log({f"bounds/{k}": v for k, v in self.bounds.items()}, step=current_timestep)
             
-            return None
+    def get_bounds(self):
+        return self.bounds
+            
+    def random_masses(self):
+        """Samples new masses, replacing one with a boundary value for evaluation."""
+        thigh_val = np.random.uniform(self.bounds["thigh_low"], self.bounds["thigh_high"])
+        leg_val = np.random.uniform(self.bounds["leg_low"], self.bounds["leg_high"])
+        foot_val = np.random.uniform(self.bounds["foot_low"], self.bounds["foot_high"])
+        body_parts_values = {"thigh": thigh_val, "leg": leg_val, "foot": foot_val}
+
+        random_part = np.random.choice(self.partOfBody)
+        boundary_to_test = f"{random_part}_{'low' if np.random.rand() < self.p_b else 'high'}"
         
-        def get_bounds(self):
-            return self.bounds
-            
-        def random_masses(self):
-            #sampling body masses 
-            thighVal = np.random.uniform(self.bounds["thigh_low"], self.bounds["thigh_high"])
-            footVal = np.random.uniform(self.bounds["foot_low"], self.bounds["foot_high"])
-            legVal = np.random.uniform(self.bounds["leg_low"], self.bounds["leg_high"])
+        body_parts_values[random_part] = self.bounds[boundary_to_test]
+        return list(body_parts_values.values()), boundary_to_test
 
-            bodyParts = {"thigh": thighVal, "foot": footVal, "leg": legVal}
-
-            pb = np.random.uniform(0,1)
-            randomCompletePart = self.set_random_parameter(bodyParts)
-            
-            if pb < self.p_b:
-                part = randomCompletePart+"_low"
-            else:
-                part = randomCompletePart+"_high"
-
-            bodyParts[randomCompletePart] = self.bounds[part]
-            return list(bodyParts.values()), part
-
-        def evaluate(self, reward, randomCompletePart):
-            self.append_reward_bodypart(partOfBody = randomCompletePart, reward = reward)
-            self.update_ADR(partOfBody = randomCompletePart)
-            return None        
+    def evaluate(self, reward: float, boundary_tested: str, current_timestep: int):
+        """Stores reward for the tested boundary and triggers an update."""
+        self.buffer[boundary_tested].append(reward)
+        self.update_ADR(partOfBody=boundary_tested, current_timestep=current_timestep)
              
-        def increase(self, partOfBody: str):
-            type = partOfBody.split('_')[0]
-            high_bound = type + '_high'
-            highOrLow = partOfBody.split('_')[1]
-            new_bound = self.bounds[partOfBody] + self.delta
-            if (highOrLow == 'low' and new_bound > self.bounds[high_bound]):
-                new_bound = new_bound - self.delta
-            self.bounds[partOfBody] = new_bound
-            return None
-        
-        def decrease(self, partOfBody: str):
-            type = partOfBody.split('_')[0]
-            low_bound = type + '_low'
-            highOrLow = partOfBody.split('_')[1]
-            new_bound = self.bounds[partOfBody] - self.delta
-            if (highOrLow == 'high' and new_bound > 0.0 and new_bound < self.bounds[low_bound]):
-                new_bound = new_bound + self.delta
-            if new_bound <= 0.0:
-                new_bound = new_bound + self.delta
-            self.bounds[partOfBody] = new_bound
-            return None
-        
-        def set_random_parameter(self, bodyParts):
-            keys = list(bodyParts.keys())
-            rand = np.random.choice(len(keys))
-            name = keys[rand]
-            return name
-        
-class ADRCallback(BaseCallback):
-        def __init__(self, handlerADR : AutomaticDomainRandomization, vec_env, eval_callback, n_envs = 1, verbose = 0, save_freq = 1000, save_path:str='./models', name_prefix:str='adr_model'):
-            super(ADRCallback, self).__init__(verbose)
-            self.adr = handlerADR
-            self.n_envs = n_envs
-            self.vec_env = vec_env
-            self.eval_callback = eval_callback
-            self.bounds_used = [None] * n_envs
-            self.n_episodes = 0
-            self.save_freq = save_freq
-            self.save_path = save_path
-            self.name_prefix = name_prefix
-        
-        def _on_step(self):
-            for done, infos, nr_env, bound_used in zip(self.locals['dones'], self.locals['infos'], range(self.vec_env.num_envs), self.bounds_used):
-                wandb.log({"step": self.num_timesteps, "entropy": self.adr.compute_entropy()})
-                if(done):
-                    self.n_episodes += 1
-                    wandb.log({"reward": infos['episode']['r'], "step": self.num_timesteps})
-                    if bound_used is not None:
-                        self.adr.evaluate(infos['episode']['r'], bound_used)
-                    env_params, self.bounds_used[nr_env] = self.adr.random_masses()
-                    self.vec_env.env_method('set_parameters', env_params, indices=nr_env)
+    def increase(self, partOfBody: str):
+        self.bounds[partOfBody] += self.delta
+        part_type, _ = partOfBody.split('_')
+        low_bound_key, high_bound_key = f"{part_type}_low", f"{part_type}_high"
+        if self.bounds[low_bound_key] > self.bounds[high_bound_key]:
+            self.bounds[low_bound_key] = self.bounds[high_bound_key]
 
+    def decrease(self, partOfBody: str):
+        self.bounds[partOfBody] -= self.delta
+        part_type, _ = partOfBody.split('_')
+        low_bound_key, high_bound_key = f"{part_type}_low", f"{part_type}_high"
+        self.bounds[partOfBody] = max(self.bounds[partOfBody], EPS)
+        if self.bounds[low_bound_key] > self.bounds[high_bound_key]:
+            self.bounds[high_bound_key] = self.bounds[low_bound_key]
+
+class ADRCallback(BaseCallback):
+    """Callback to orchestrate Automatic Domain Randomization during training."""
+    def __init__(self, handlerADR: AutomaticDomainRandomization, verbose=0):
+        super(ADRCallback, self).__init__(verbose)
+        self.adr = handlerADR
+        self.bounds_used = [None] * self.training_env.num_envs
+
+    def _on_step(self):
+        wandb.log({"adr_entropy": self.adr.compute_entropy()}, step=self.num_timesteps)
+        
+        for i in range(self.training_env.num_envs):
+            if self.locals['dones'][i]:
+                info = self.locals['infos'][i]
+                boundary_tested = self.bounds_used[i]
+                if boundary_tested is not None:
+                    episode_reward = info['episode']['r']
+                    self.adr.evaluate(episode_reward, boundary_tested, self.num_timesteps)
+                
+                env_params, self.bounds_used[i] = self.adr.random_masses()
+                self.training_env.env_method('set_parameters', env_params, indices=[i])
+        return True
--- train_ppo_adr.py
+++ train_ppo_adr.py
@@ -1,19 +1,23 @@
+# train_ppo_adr_sweep.py
+
 import os
-import gym
 import time
+import gym
 import wandb
 import numpy as np
 import argparse
+
+# Ensure custom_hopper.py is in ./env/ and the folder has an __init__.py file
+from env.custom_hopper import CustomHopper
 from ADR import AutomaticDomainRandomization, ADRCallback
+
 from stable_baselines3 import PPO
-from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.vec_env import DummyVecEnv, VecMonitor
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.callbacks import EvalCallback, CallbackList
+from stable_baselines3.common.callbacks import BaseCallback, EvalCallback, CallbackList
 from stable_baselines3.common.evaluation import evaluate_policy
 
-LOG_DIR = "./logs_ADR"
-
+# --- Constants and Hyperparameters ---
 BEST_PARAMS = {
     "learning_rate": 0.0007081485369506237,
     "clip_range": 0.26785576292805646,
@@ -28,137 +32,116 @@ THIGH_MEAN_MASS = 3.92699082
 LEG_MEAN_MASS = 2.71433605
 FOOT_MEAN_MASS = 5.0893801
 
+# --- W&B Sweep Configuration ---
+sweep_configuration = {
+    "method": "grid",
+    "name": "ADR-Delta-Sweep",
+    "metric": {"name": "eval/mean_reward", "goal": "maximize"},
+    "parameters": {
+        "delta": {"values": [0.02, 0.05, 0.1]},
+    }
+}
+
+class DetailedLoggingCallback(BaseCallback):
+    """Logs detailed episode data to W&B for plotting."""
+    def __init__(self, window_size=100, verbose=0):
+        super().__init__(verbose)
+        self.window_size = window_size
+        self.recent_rewards = []
+        self.start_time = time.time()
+
+    def _on_step(self) -> bool:
+        for i, done in enumerate(self.locals['dones']):
+            if done:
+                info = self.locals['infos'][i]
+                if "episode" in info:
+                    ep_info = info["episode"]
+                    reward, length = ep_info["r"], ep_info["l"]
+                    
+                    self.recent_rewards.append(reward)
+                    if len(self.recent_rewards) > self.window_size: self.recent_rewards.pop(0)
+                    
+                    log_data = {"reward/episode_reward": reward, "episode_length": length}
+                    if len(self.recent_rewards) == self.window_size:
+                        log_data["reward/rolling_mean_100"] = np.mean(self.recent_rewards)
+                    
+                    wandb.log(log_data, step=self.num_timesteps)
+        return True
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--device', type=str, default='cuda', choices=['cpu', 'cuda'], help='Device to train on')
+    n_envs = os.cpu_count() or 1
+    parser.add_argument('--p-b', default=0.5, type=float, help='Probability of testing a boundary.')
+    parser.add_argument('--m', default=50, type=int, help='ADR buffer size.')
+    parser.add_argument('--low-th', default=1000, type=int, help='Lower performance threshold.')
+    parser.add_argument('--high-th', default=1500, type=int, help='Upper performance threshold.')
+    parser.add_argument('--n-envs', default=n_envs, type=int, help='Number of parallel environments.')
+    parser.add_argument('--timesteps', default=3_000_000, type=int, help='Total training timesteps.')
+    parser.add_argument('--save-path', default='./models_ADR_Sweep/', type=str, help='Path to save models.')
+    parser.add_argument('--device', default='auto', type=str, help='Device to use (cpu, cuda).')
     return parser.parse_args()
 
+def objective(config=None):
+    with wandb.init(config=config):
+        config = wandb.config
+        args = parse_args()
 
-class WandADRCallback(EvalCallback):
-    def __init__(self, print_every=1000, verbose=1):
-        super().__init__(Monitor(gym.make("CustomHopper-adr-v0")), n_eval_episodes=50, eval_freq=50000,
-                         best_model_save_path="./best_eval_adr/", deterministic=True, render=False)
-        self.print_every = print_every
-        self.episode_rewards = []
-        self.recent_rewards = []
-        self.smoothed = []
-        self.variance = []
-        self.cumulative_sim = 0.0
-        self.cumulative_wall = 0.0
-        self.cumulative_sim_times = []
-        self.cumulative_wall_times = []
-        self.wall_times = []
-        self.episode_lengths = []
-        self._episode_start_time = None
-        self.window = 100
-        os.makedirs(LOG_DIR, exist_ok=True)
+        # --- Environment Setup ---
+        # CRITICAL: Use 'CustomHopper-adr-v0' and wrap in VecMonitor
+        train_env = make_vec_env("CustomHopper-adr-v0", n_envs=args.n_envs, vec_env_cls=DummyVecEnv)
+        train_env = VecMonitor(train_env)
+        eval_env = gym.make("CustomHopper-target-v0")
 
-    def _on_step(self) -> bool:
-        if self._episode_start_time is None:
-            self._episode_start_time = time.time()
+        # --- ADR Handler ---
+        init_params = {"thigh": THIGH_MEAN_MASS, "leg": LEG_MEAN_MASS, "foot": FOOT_MEAN_MASS}
+        handlerADR = AutomaticDomainRandomization(
+            init_params, p_b=args.p_b, m=args.m, delta=config.delta,
+            thresholds=[args.low_th, args.high_th]
+        )
 
-        for info in self.locals["infos"]:
-            ep = info.get("episode")
-            if ep:
-                reward = ep["r"]
-                steps = ep["l"]
-                sim_time = steps * 0.008
-                wall_time = time.time() - self._episode_start_time
-                self._episode_start_time = None
+        # --- Callbacks ---
+        delta_save_path = os.path.join(args.save_path, f"delta_{config.delta}")
+        best_model_path = os.path.join(delta_save_path, "best_model")
+        
+        adr_callback = ADRCallback(handlerADR=handlerADR)
+        logging_callback = DetailedLoggingCallback()
+        # EvalCallback logs to wandb automatically under "eval/" prefix
+        eval_callback = EvalCallback(
+            eval_env, best_model_save_path=best_model_path,
+            log_path=delta_save_path, eval_freq=max(25000 // args.n_envs, 1),
+            n_eval_episodes=50, deterministic=True,
+        )
+        callbacks = CallbackList([adr_callback, logging_callback, eval_callback])
 
-                self.episode_rewards.append(reward)
-                self.recent_rewards.append(reward)
-                self.wall_times.append(wall_time)
-                self.episode_lengths.append(sim_time)
-                self.cumulative_sim += sim_time
-                self.cumulative_wall += wall_time
-                self.cumulative_sim_times.append(self.cumulative_sim)
-                self.cumulative_wall_times.append(self.cumulative_wall)
-
-                if len(self.recent_rewards) > self.window:
-                    self.recent_rewards.pop(0)
-
-                smoothed = np.mean(self.recent_rewards) if len(self.recent_rewards) == self.window else None
-                var = np.var(self.recent_rewards) if smoothed is not None else None
-
-                if smoothed is not None:
-                    self.smoothed.append(smoothed)
-                    self.variance.append(var)
-
-                wandb.log({
-                    "reward_per_episode": reward,
-                    "rolling_reward_mean_100": smoothed,
-                    "rolling_reward_var_100": var,
-                    "cumulative_sim_time": self.cumulative_sim,
-                    "cumulative_wall_time": self.cumulative_wall
-                }, step=self.num_timesteps)
-
-                if self.verbose and len(self.episode_rewards) % self.print_every == 0:
-                    print(f"--- Episode {len(self.episode_rewards)} ---")
-                    print(f"  Reward: {reward:.2f}")
-                    print(f"  Smoothed: {smoothed:.2f}" if smoothed else "")
-                    print(f"  Sim Time: {sim_time:.2f}s | Wall Time: {wall_time:.2f}s")
-        return True
-
-    def _on_training_end(self) -> None:
-        np.save(os.path.join(LOG_DIR, 'episode_rewards.npy'), self.episode_rewards)
-        np.save(os.path.join(LOG_DIR, 'rolling_reward_mean_100.npy'), self.smoothed)
-        np.save(os.path.join(LOG_DIR, 'rolling_reward_var_100.npy'), self.variance)
-        np.save(os.path.join(LOG_DIR, 'cumulative_sim_time.npy'), self.cumulative_sim_times)
-        np.save(os.path.join(LOG_DIR, 'cumulative_wall_time.npy'), self.cumulative_wall_times)
-        np.save(os.path.join(LOG_DIR, 'episode_times_simulated.npy'), self.episode_lengths)
-        np.save(os.path.join(LOG_DIR, 'episode_times_wallclock.npy'), self.wall_times)
-
+        # --- Model ---
+        model = PPO('MlpPolicy', train_env, verbose=0, device=args.device, **BEST_PARAMS)
+        
+        # --- Training ---
+        model.learn(total_timesteps=args.timesteps, callback=callbacks, progress_bar=True)
 
 def main():
+    # Login to W&B
+    wandb.login()
+
+    # Create the sweep
+    sweep_id = wandb.sweep(sweep=sweep_configuration, project="Hopper_ADR_Final_Sweep")
+    
+    # Run the sweep agent
+    wandb.agent(sweep_id, function=objective, count=3) # Runs for all 3 delta values
+
+    # --- Final Evaluation After Sweep ---
+    print("\n\n--- Final Evaluation on Target Environment ---")
     args = parse_args()
-    wandb.init(project="PPO_ADR_Hopper")
-
-    env_id = "CustomHopper-adr-v0"
-    train_env = make_vec_env(env_id, n_envs=1, vec_env_cls=DummyVecEnv)
-    test_env = gym.make(env_id)
-
-    init_params = {"thigh": THIGH_MEAN_MASS, "leg": LEG_MEAN_MASS, "foot": FOOT_MEAN_MASS}
-    adr_handler = AutomaticDomainRandomization(
-        init_params=init_params,
-        p_b=0.5,
-        m=50,
-        delta=0.05,
-        thresholds=[1000, 1500]
-    )
-
-    train_env.set_attr("bounds", adr_handler.get_bounds())
-
-    eval_callback = WandADRCallback()
-    adr_callback = ADRCallback(
-        handlerADR=adr_handler,
-        vec_env=train_env,
-        eval_callback=eval_callback,
-        n_envs=1,
-        verbose=1,
-        save_freq=50000,
-        save_path="./models_ADR"
-    )
-
-    model = PPO(
-        policy="MlpPolicy",
-        env=train_env,
-        verbose=1,
-        learning_rate=BEST_PARAMS["learning_rate"],
-        clip_range=BEST_PARAMS["clip_range"],
-        ent_coef=BEST_PARAMS["ent_coef"],
-        n_steps=BEST_PARAMS["n_steps"],
-        batch_size=BEST_PARAMS["batch_size"],
-        gamma=BEST_PARAMS["gamma"],
-        gae_lambda=BEST_PARAMS["gae_lambda"],
-        device=args.device
-    )
-
-    model.learn(total_timesteps=3_000_000, callback=CallbackList([adr_callback, eval_callback]), progress_bar=True)
-
-    wandb.finish()
-
+    for delta in sweep_configuration["parameters"]["delta"]["values"]:
+        model_path = os.path.join(args.save_path, f"delta_{delta}", "best_model", "best_model.zip")
+        if os.path.exists(model_path):
+            print(f"Evaluating model for delta = {delta}")
+            test_env = gym.make('CustomHopper-target-v0')
+            model = PPO.load(model_path)
+            mean_reward, std_reward = evaluate_policy(model, test_env, n_eval_episodes=100, render=False, warn=False)
+            print(f"[s-t] Mean Reward (delta={delta}): {mean_reward:.2f} +/- {std_reward:.2f}")
+        else:
+            print(f"Model for delta = {delta} not found at {model_path}")
 
 if __name__ == '__main__':
     main()
